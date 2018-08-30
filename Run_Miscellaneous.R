ea3c677f67a4ff38dfdaec2f59104580932fe7fc
Richard White <r@rwhite.no>
2018-08-30 11:01:56 +0300

    graphs2

--- Run_Miscellaneous.R
+++ Run_Miscellaneous.R
@@ -22,9 +22,16 @@ library(ggplot2)
 
 long <- melt.data.table(toPlot,id.vars = c("year","week", "xValue"))
 
+breaks <- unique(long[,c("year","week","xValue")])
+breaks <- breaks[week %in% c(1,26)]
+breaks[,label:=sprintf("%s-%s",year,week)]
+
+print(breaks)
+
 p <- ggplot(data=long, mapping=aes(x=xValue,y=value))
 p <- p + geom_bar(stat="identity")
 p <- p + facet_wrap(~variable)
+p <- p + scale_x_continuous("Date",labels=breaks$label,breaks=breaks$xValue)
 p
 
 
