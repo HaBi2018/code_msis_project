b6085538b875059c1b839557e4e5541b78b51824
Richard White <r@rwhite.no>
2018-08-30 10:51:49 +0300

    graphs

--- Run_Miscellaneous.R
+++ Run_Miscellaneous.R
@@ -9,12 +9,23 @@ fullDataSVM[,.(
 
 #Graph
 
-fullDataSVM[,.(
+toPlot <- fullDataSVM[,.(
   numS=sum(s_status!="Normal"), 
   numM=sum(msis_outbreak==T),
   numV=sum(vesuv_outbreak==1)
 ),by=.(year,week)]
 
+setorder(toPlot,year,week)
+toPlot[,xValue:=1:nrow(toPlot)]
+
+library(ggplot2)
+
+long <- melt.data.table(toPlot,id.vars = c(year,week))
+
+p <- ggplot(data=long, mapping=aes(x=xValue,y=value))
+p <- p + geom_bar(stat="identity")
+p <- p + facet_wrap(~variable)
+p
 
 
 # Yearly grafs showing outbreak signals/n pr system pr week pr year
