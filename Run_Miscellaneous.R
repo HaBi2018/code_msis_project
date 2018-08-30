c77b9abacc227935cb40e57fb6ef9c52bc94f443
BaKaVa <hannebirgitte@hotmail.com>
2018-08-30 15:02:19 +0200

    n

--- Run_Miscellaneous.R
+++ Run_Miscellaneous.R
@@ -23,16 +23,47 @@ library(ggplot2)
 long <- melt.data.table(toPlot,id.vars = c("year","week", "xValue"))
 
 breaks <- unique(long[,c("year","week","xValue")])
-breaks <- breaks[week %in% c(1,26)]
+breaks <- breaks[week %in% c(1)]
 breaks[,label:=sprintf("%s-%s",year,week)]
 
 print(breaks)
 
 p <- ggplot(data=long, mapping=aes(x=xValue,y=value))
 p <- p + geom_bar(stat="identity")
-p <- p + facet_wrap(~variable)
+p <- p + facet_wrap(~variable,ncol=1)
 p <- p + scale_x_continuous("Date",labels=breaks$label,breaks=breaks$xValue)
 p
 
 
-# Yearly grafs showing outbreak signals/n pr system pr week pr year
+
+
+
+##############################################
+# Lagre tabelloversikt for signaler i excel
+
+results<-fullDataSVM[,.(
+  numS=sum(s_status!="Normal"), 
+  numM=sum(msis_outbreak==T),
+  numV=sum(vesuv_outbreak==1)
+),by=.(year)]
+
+na.omit(results)
+
+openxlsx::write.xlsx(na.omit(results), file=file.path("C:/Users/Hanne/Documents/NMBU/Masteroppgave/Analayser i R/180830","signaler.xlsx"))
+
+
+
+
+###############################################
+# Aggregate all "double" rows in Vesuv by municp, year, week, and sum Antall
+
+# Dataset Vesuv = vFull
+nrow(vFull) #nrow = 537
+
+#vFull<- tapply(vFull$Antall, vFull[, c("location", "year","week","vesuv_outbreak" )], sum)
+
+
+#vFull <-vFull[,.(vFull$Antall),by=.(location, year,week, vesuv_outbreak,Antall)]
+nrow(vFull) #nrow=533
+
+vFull <-vFull[,.(vFull$Antall),by=.(location, year,week, vesuv_outbreak)]
