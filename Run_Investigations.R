2276e3d4d92282f72c5ac9f578d7c0c5dc7f6663
BaKaVa <hannebirgitte@hotmail.com>
2018-09-20 16:22:31 +0200

    a

---
+++ Run_Investigations.R
@@ -0,0 +1,43 @@
+
+## INVESTIGATION - KNOWN OUTBREAKS IN VESUV
+
+# Outbreak Ringerike, 2012, week 50
+fullDataSVM[location=="municip0605" & (year==2012 & week==50)]
+
+#Outbreak Røros, 2007, week 20
+fullDataSVM[location=="municip5025" & (year==2007 & week==20)]
+
+
+#Outbreak Ringerike, 2017, week 11
+fullDataSVM[location=="municip0605" & (year==2017 & week==11)]
+
+#Outbreak Ringerike, 2017, week 11, lag +/- one week
+fullDataSVM[location=="municip0605" & (year==2017 & week==10)]
+fullDataSVM[location=="municip0605" & (year==2017 & week==12)]
+
+
+#Outbreak Ullensaker, 2008, week 39
+fullDataSVM[location=="municip0235" & (year==2008 & week==39)]
+
+#Outbreak Hamar, 2008, week 36
+fullDataSVM[location=="municip0403" & (year==2008 & week==36)]
+
+#Outbreak Målselv, 2014, week 19
+fullDataSVM[location=="municip1924" & (year==2014 & week==19)]
+
+#Outbreak Ålesund, 2013, week 9
+fullDataSVM[location=="municip1504" & (year==2013 & week==9)]
+
+#Outbreak Stavanger, 2007, week 50
+fullDataSVM[location=="municip1103" & (year==2007 & week==50)]
+
+
+#Outbreak Målselv, 2015, week 9
+fullDataSVM[location=="municip1924" & (year==2015 & week==9)]
+
+#Outbreak Målselv, 2015, week 9, lag +/- one week
+fullDataSVM[location=="municip1924" & (year==2015 & week==8)]
+fullDataSVM[location=="municip1924" & (year==2015 & week==10)]
+
+
+
